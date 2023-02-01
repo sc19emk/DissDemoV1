9dcbf5700a43e7b7926fbeb22c27be7110284e8d
Emily Kerkhof <60623969+sc19emk@users.noreply.github.com>
2023-02-01 15:27:27 +0000

    Initial Commit

---
+++ DissDemoV1/DissDemoV1App.swift
@@ -0,0 +1,17 @@
+//
+//  DissDemoV1App.swift
+//  DissDemoV1
+//
+//  Created by Emily Kerkhof on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct DissDemoV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
