bbb1bf60fde6f1422120fc57cdd03248bfc45b59
Emily Kerkhof <60623969+sc19emk@users.noreply.github.com>
2023-02-23 10:38:29 +0000

    Adding Alarm Functionality
    Begin of sprint 2

--- DissDemoV1/AlarmView.swift
+++ DissDemoV1/AlarmView.swift
@@ -6,16 +6,49 @@
 //
 
 import SwiftUI
-import AVFoundation
+import Foundation
+import AVFoundation // library for playing sound effects
 
+var audioPlayer = AVAudioPlayer() // variable for sound effect
+var playing = false
 
 struct AlarmView: View {
     var body: some View {
         VStack{
             Text("Alarm Page").font(.title).bold()
-            Text("Loud SOS alarm - to attract attention. - When pressed could record location- send an alert to friends.- **After 10 seconds calls 999?**")
+            //Text("Loud SOS alarm - to attract attention. - When pressed could record location- send an alert to friends.- **After 10 seconds calls 999?**")
                 .multilineTextAlignment(.center)
+
+            Button("Alarm!") {
+                playSound()
+            }
+                .controlSize(.large)
+                .buttonStyle(.borderedProminent)
+                .accentColor(.red)
+            
+            Button("stop alarm") {
+                stopSound()
+            }
+                .controlSize(.large)
+                .buttonStyle(.borderedProminent)
+                .accentColor(.blue)
         }
+    }
+    func playSound() {
+        let path = Bundle.main.path(forResource: "alarmSound.mp3", ofType: nil)!
+        let url = URL(fileURLWithPath: path)
+        do {
+            // create your audioPlayer in your parent class as a property
+            // audioPlayer.numberOfLoops = -1 isnt working...?
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer.play()
+            audioPlayer.numberOfLoops = -1
+        } catch {
+            print("couldn't load the file")
+        }
+    }
+    func stopSound() {
+        audioPlayer.stop()
     }
 }
 
